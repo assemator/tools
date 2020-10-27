6cbd603590a3a151dc13bb1db72a0f8ddf90b28d
assemator <48603678+assemator@users.noreply.github.com>
2020-10-27 18:34:27 +0200

    Update download_wg.sh

--- download_wg.sh
+++ download_wg.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
+while getopts "u:" arg; do
+  case ${arg} in
+    u)
+      GITHUB_USER=${OPTARG}
+      ;;
+    ?)
+      echo "Invalid option: -${OPTARG}."
+      exit 1
+      ;;
+  esac
+done
+
 curl -O https://raw.githubusercontent.com/assemator/tools/master/centos7_wireguard.sh
 chmod +x centos7_wireguard.sh
 ./centos7_wireguard.sh
